56b6c35944e49fd01b0be46eeb61ca27bcd6f711
Sergio Junior <sergio.ltnj@gmail.com>
2022-04-06 16:14:05 -0300

    Tela de login finalizada

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,85 @@
+﻿import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  final _globalKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Entrar"),
+        centerTitle: true,
+        actions: <Widget>[
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(shadowColor: Colors.transparent),
+            onPressed: () {},
+            child: const Text(
+              "CRIAR CONTA",
+              style: TextStyle(fontSize: 15),
+            ),
+          ),
+        ],
+      ),
+      body: Form(
+        key: _globalKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(hintText: "E-mail"),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text) {
+                if (text == null || !text.contains("@")) {
+                  return "E-mail inválido";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(hintText: "Senha"),
+              obscureText: true,
+              validator: (text) {
+                if (text == null || text.length < 6) {
+                  return "Senha inválida";
+                } else {
+                  return null;
+                }
+              },
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                child: const Padding(
+                  child: Text("Esqueci minha senha"),
+                  padding: EdgeInsets.only(top: 10),
+                ),
+                onTap: () {},
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              height: 44,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_globalKey.currentState!.validate()) {}
+                },
+                child: const Text(
+                  "Entrar",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
